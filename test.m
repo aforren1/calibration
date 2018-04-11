5d8ff3f2845e21680388bc822576da222d31b4d5
Alex Forrence <aforren1@jhu.edu>
2018-04-11 16:38:57 -0400

    Touch up examples

--- test.m
+++ test.m
@@ -1,12 +1,12 @@
+% just noise, but example of extracting data
+hinfo = h5info('demo.hdf5');
 
-hinfo = h5info('test.hdf5');
+h5disp('demo.hdf5', hinfo.Groups(2).Name)
 
-dset = h5read(hinfo.Groups.Name.Datasets.Name(0));
-
-h5disp('test.hdf5', hinfo.Groups.Name)
-
-data = h5read('test.hdf5', strcat(hinfo.Groups.Name, '/forces'));
+data = h5read('demo.hdf5', strcat(hinfo.Groups(2).Name, '/forces'));
 
 plot(data')
 
-data2 = h5read('test.hdf5', strcat(hinfo.Groups.Name, '/voltages'));
+data2 = h5read('demo.hdf5', strcat(hinfo.Groups(2).Name, '/voltages'));
+
+plot(data2')
